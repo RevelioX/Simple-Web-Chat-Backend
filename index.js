0c26b3b36464046442bdaa63ef3b343977157aaf
Mariano Brarda <64959782+RevelioX@users.noreply.github.com>
2022-12-27 05:38:52 -0300

    Added Sockets

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ const router = require("./routers/message");
 const connectDB = require("./db/connect");
 const app = express();
 const bodyParser = require("body-parser");
+const {createServer} = require("http");
+const {Server} = require("socket.io");
 
 const jsonParser = bodyParser.json(); 
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer,{
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+})
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 async function startApp(){
     try{
         await connectDB()
-        app.listen(4000);
-        console.log("Server listening on 4000");
+        httpServer.listen(3080);
+        console.log("Server listening on 3080");
     }
     catch(e){
         console.log(e);
@@ -21,7 +34,11 @@ async function startApp(){
 }
 
 startApp();
-app.use("/chat",jsonParser,router)
+app.use("/messages",jsonParser,router)
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+io.on("connection", (socket) => {console.log("Usuario Conectado")
+socket.on("message",(msg) => {
+    console.log(JSON.parse(msg))
+})})
